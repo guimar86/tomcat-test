bbfc86f864aef198bb9f88e452e5610a1e3bd7f3
Renato Martins <quessuadev@gmail.com>
2022-02-19 05:26:56 +0100

    add  gitignore

---
+++ src/main/java/com/simplilearn/tomcat/TomcatMasterApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simplilearn/tomcat/TomcatMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.tomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
